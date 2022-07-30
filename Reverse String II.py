717bfc5efc61b69763c48c8eeda70c166a9e168d
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-07-29 20:42:57 -0700

    Add files via upload

--- Reverse String II.py
+++ Reverse String II.py
@@ -10,3 +10,9 @@ class Solution:
         for i in range(0, len(s), 2*k):
             s[i:i+k] = reversed(s[i:i+k])
         return "".join(s)
+
+        # Solution 2
+        s = [s[i:i+k] for i in range(0, len(s), k)]
+        for i in range(0, len(s), 2):
+            s[i] = s[i][::-1]
+        return ''.join(s)
